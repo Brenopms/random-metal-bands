0378b51dded3ba8c0e8cc4848fc18ba11feb3a50
Breno <brenosilva1@GMAIL.COM>
2018-06-02 21:19:51 -0300

    fetch data and put in band object

--- js/fetch.js
+++ js/fetch.js
@@ -1,8 +1,41 @@
-let getAlbum = documento.querySelector('.get-album');
+let getAlbum = document.querySelector('.get-album');
 let apiKey = config.API_KEY;
+let teste;
+let display;
 
-fetch('https://myexample.com', {
-    method: 'POST',
-    headers: {'Content-Type':'application/x-www-form-urlencoded'}, 
-    body: 'foo=bar&blah=1'
-})
+class Band {
+    constructor(name, location, status, formed, years, genre, photo, logo){
+        this.name = name;
+        this.location = location;
+        this.status = status;
+        this.formed = formed;
+        this.years = years;
+        this.genre = genre;
+        this.photoURL = photo;
+        this.logoURL = logo;
+    }
+}
+
+fetch(`http://em.wemakesites.net/band/random?api_key=${apiKey}`)
+    .then(response => {
+        response.json().then(responseData => {
+            console.log(responseData.data.band_name);
+            let band = new Band(
+                responseData.data.band_name,
+                responseData.data.details.location,
+                responseData.data.details.status,
+                responseData.data.details["formed in"],
+                responseData.data.details["years active"],
+                responseData.data.details.genre,
+                responseData.data.photo,
+                responseData.data.logo
+            );
+            console.log(band)
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })   
+
+
+    //`http://em.wemakesites.net/band/random?api_key=${apiKey}`
